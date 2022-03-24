c42dfe6581526097fa06af8756a5ef574ef9ad71
matinico <mati.nico.fernandez@gmail.com>
2022-03-23 23:16:47 -0300

    Create Ejercicio Nº1.m

---
+++ Ejercicio Nº1.m
@@ -0,0 +1,18 @@
+function [Tc] = CtoF(t)
+    Tc = (t*9/5)+32;
+end;
+
+function [Tf] = FtoC(t)
+    Tf = (t -32)*5/9;
+end;
+
+function [Wmph] = KpHtoMpH(w)
+    Wmph = w/1.60934;
+end;
+
+function [St] = getThermalSensation(t, w)
+    T = CtoF(t);
+    V = KpHtoMpH(w);
+    st = 35.74 + 0.6215*T - 35.75*(V**0.16) + 0.4275*T*(V**0.16);
+    St = int(FtoC(st));
+end;
