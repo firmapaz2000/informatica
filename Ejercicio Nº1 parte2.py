97fbb021c4bac460044194236ca42d00f96d7bd9
matinico <mati.nico.fernandez@gmail.com>
2022-03-26 17:59:18 -0300

    Minor Bugs Fixed

--- Ejercicio Nº1 parte2.py
+++ Ejercicio Nº1 parte2.py
@@ -1,11 +1,6 @@
-def CtoF(t):
-    return (t*9/5)+32
-
-def FtoC(t):
-    return (t -32)*5/9
-
-def KpHtoMpH(w):
-    return w/1.60934
+CtoF = lambda t : (t*9/5)+32
+FtoC = lambda t : (t -32)*5/9
+KpHtoMpH = lambda w : w/1.60934
 
 def getThermalSensation(t, w):
     T = CtoF(t)
@@ -13,15 +8,15 @@ def getThermalSensation(t, w):
     st = 35.74 + 0.6215*T - 35.75*(V**0.16) + 0.4275*T*(V**0.16)
     return int(FtoC(st))
 
+
 #Esto solo se cumple si se llama por si mismo y no como parte
 #de un programa mas grande
 if __name__ == "__main__":
     t = input("Introduzca la temperatura en °C: ")
     w = input("Introduzca la velocidad del viento en km/h: ")
     printText = ""
-    if (t.isnumeric()):
+    if (not t.isnumeric()):
         if(w.isnumeric()):
-
             errorText = "La sensacion termica es: {} °C"
             printText = errorText.format(getThermalSensation(int(t), int(w)))
         else:
--- Ejercicio Nº1.m
+++ Ejercicio Nº1.m
@@ -1,9 +1,9 @@
 function [Tc] = CtoF(t)
-    Tc = (t*9/5)+32;
+    Tc = (t * 9/5) + 32;
 end;
 
 function [Tf] = FtoC(t)
-    Tf = (t -32)*5/9;
+    Tf = (t - 32) * 5/9;
 end;
 
 function [Wmph] = KpHtoMpH(w)
@@ -14,5 +14,5 @@ function [St] = getThermalSensation(t, w)
     T = CtoF(t);
     V = KpHtoMpH(w);
     st = 35.74 + 0.6215*T - 35.75*(V**0.16) + 0.4275*T*(V**0.16);
-    St = int(FtoC(st));
+    St = FtoC(st);
 end;
