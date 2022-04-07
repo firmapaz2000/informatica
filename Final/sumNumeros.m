54dc1512bdf098704fd0c571a98ef914cb80871a
nicolass00 <mati.nico.fernandez@gmail.com>
2022-04-07 17:03:20 -0300

    Add files via upload

---
+++ Final/sumNumeros.m
@@ -0,0 +1,12 @@
+function [sum] = sumNumeros(x)
+  
+  z=num2str (x);
+  i=1;
+  sum=0;
+  
+  while i <= length(z)
+    sum = sum + str2num(z(i));
+    i=i+1;
+  endwhile
+  
+endfunction
---
+++ Final/valorMatriz.m
@@ -0,0 +1,24 @@
+function [valor,x,y] = valorMatriz(m)
+  i = 1;
+  v=size(m)
+  
+  valor=[];
+  x=[];
+  y=[];
+  
+  while i <= v(1)
+    j = 1;
+    while j <= v(2)
+      value = m(i,j);
+      
+      if value ~= 0
+        valor=[valor, value];
+        x=[x, j];
+        y=[y, i];
+      endif
+      
+      j=j+1;
+    endwhile
+    i=i+1;
+  endwhile
+endfunction
