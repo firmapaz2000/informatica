8085cee27c4e90c89488ae08617828c940cee7e0
nicolass00 <mati.nico.fernandez@gmail.com>
2022-04-07 17:27:03 -0300

    Add files via upload

--- Final/sumNumeros.m
+++ Final/sumNumeros.m
@@ -1,6 +1,6 @@
 function [sum] = sumNumeros(x)
   
-  z=num2str (x);
+  z=num2str(x);
   i=1;
   sum=0;
   
@@ -8,5 +8,9 @@ function [sum] = sumNumeros(x)
     sum = sum + str2num(z(i));
     i=i+1;
   endwhile
+  
+  if length(num2str(sum)) > 1
+    sum = sumNumeros(sum);
+  endif
   
 endfunction
