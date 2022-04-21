7c3d6ed89c718853ea62decf1b69ea75dc006c48
firmapaz2000 <103159760+firmapaz2000@users.noreply.github.com>
2022-04-20 23:03:49 -0300

    Update FibonacciZ.m

--- FibonacciZ.m
+++ FibonacciZ.m
@@ -2,6 +2,6 @@ function[y]=FibonacciZ(n)
 y=[];
 i=1;
 while i<=n
-    y(i)=(1/sqrt(5))*(((1+sqrt(5))/2)^(i+1)-((1-sqrt(5))/2)^(i+1));
+    y(i)=fix((1/sqrt(5))*(((1+sqrt(5))/2)^(i+1)-((1-sqrt(5))/2)^(i+1)));
     i=i+1;
 end
